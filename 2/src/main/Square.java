db0c9097269dc60b90886631c6508b6314e57161
amsabour <amsabour79@gmail.com>
2021-03-17 19:36:58 +0330

    Filled in Square class

--- 2/src/main/Square.java
+++ 2/src/main/Square.java
@@ -1,20 +1,27 @@
 package main;
 
 public class Square {
+    private int sideLength;
 
     public Square(int sideLength) {
+        if (sideLength <= 0)
+            throw new RuntimeException("Square side length should be positive.");
 
+        this.sideLength = sideLength;
     }
 
     public void setSideLength(int sideLength) {
+        if (sideLength <= 0)
+            throw new RuntimeException("Square side length should be positive.");
 
+        this.sideLength = sideLength;
     }
 
     public int getSideLength() {
-        return 0;
+        return sideLength;
     }
 
     public int computeArea() {
-        return 0;
+        return getSideLength() * getSideLength();
     }
 }

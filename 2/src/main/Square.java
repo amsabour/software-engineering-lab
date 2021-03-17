51ef568b57a5f68fd2900679979c96ed3690bafd
amsabour <amsabour79@gmail.com>
2021-03-17 19:34:46 +0330

    Added tests for Square class

---
+++ 2/src/main/Square.java
@@ -0,0 +1,20 @@
+package main;
+
+public class Square {
+
+    public Square(int sideLength) {
+
+    }
+
+    public void setSideLength(int sideLength) {
+
+    }
+
+    public int getSideLength() {
+        return 0;
+    }
+
+    public int computeArea() {
+        return 0;
+    }
+}
